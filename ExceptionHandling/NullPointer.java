970b6c25c611c98cbc6433fd4c340ffa50c0554c
KavitaJangid12 <kavitajangid1205@gmail.com>
2026-01-31 10:58:36 +0530

    Exception files

---
+++ ExceptionHandling/NullPointer.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class NullPointer {
+	public static void main(String[] args) {
+		String name=null;
+		try {
+			System.out.println(name.length());
+		}
+		catch(NullPointerException ae) {
+			System.out.println(ae);
+
+		}
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+	}
+
+}
---
+++ ExceptionHandling/StringIndexOutOfBoundsEx.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class StringIndexOutOfBoundsEx {
+
+	public static void main(String[] args) {
+		String name="ramu";
+		System.out.println(name.charAt(0));
+		System.out.println(name.charAt(3));
+
+		try {
+		System.out.println(name.charAt(10));
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+
+		
+	}
+
+}
